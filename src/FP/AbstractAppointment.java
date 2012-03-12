2b7bef3f691e7a66f0c9752669b2496f051d13ee
lycantrophe <wolfhood@gmail.com>
2012-03-12 10:54:59 +0100

    Added getters and updateAppointment

--- src/FP/AbstractAppointment.java
+++ src/FP/AbstractAppointment.java
@@ -12,19 +12,94 @@ import java.util.Date;
  * @author lycantrophe
  */
 public abstract class AbstractAppointment implements Appointment {
-    
-    protected Date start;
-    protected Date end;
-    protected Person owner;
-    protected String description;
-    protected AbstractLocation location;
-    protected ArrayList<String> participants;
-    
-    public AbstractAppointment( Person owner, Date start, Date end, String description, ArrayList<String> participants ){
+
+    private Date start;
+    private Date end;
+    private Person owner;
+    private String description;
+    private AbstractLocation location;
+    private ArrayList<String> participants;
+
+    public AbstractAppointment(Person owner, Date start, Date end, String description, ArrayList<String> participants) {
         this.owner = owner;
         this.start = start;
         this.end = end;
         this.description = description;
         this.participants = participants;
+    }
+
+    /**
+     * Updates the appointment according to differences (and not-null fields) in
+     * newAppointment
+     *
+     * @param newAppointment Appointment object holding fields to modify
+     */
+    public void updateAppointment(Appointment newAppointment) {
+        if (newAppointment.getStart() != null) {
+            start = newAppointment.getStart();
+        }
+        if (newAppointment.getEnd() != null) {
+            end = newAppointment.getEnd();
+        }
+        if (newAppointment.getOwner() != null) {
+            owner = newAppointment.getOwner();
+        }
+        if (newAppointment.getDescription() != null) {
+            description = newAppointment.getDescription();
+        }
+        if (newAppointment.getLocation() != null) {
+            location = newAppointment.getLocation();
+        }
+        if (newAppointment.getParticipants() != null) {
+            participants = newAppointment.getParticipants();
+        }
+    }
+
+    /**
+     * @return the start
+     */
+    @Override
+    public Date getStart() {
+        return start;
+    }
+
+    /**
+     * @return the end
+     */
+    @Override
+    public Date getEnd() {
+        return end;
+    }
+
+    /**
+     * @return the owner
+     */
+    @Override
+    public Person getOwner() {
+        return owner;
+    }
+
+    /**
+     * @return the description
+     */
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the location
+     */
+    @Override
+    public AbstractLocation getLocation() {
+        return location;
+    }
+
+    /**
+     * @return the participants
+     */
+    @Override
+    public ArrayList<String> getParticipants() {
+        return participants;
     }
 }
