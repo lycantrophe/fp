0b6d4dceb03a4d060fdeb4e458a19d94b772f3a4
lycantrophe <wolfhood@gmail.com>
2012-03-13 19:02:55 +0100

    updateAppointment query.

--- src/FP/Query.java
+++ src/FP/Query.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -23,12 +24,29 @@ public class Query {
                 "jdbc:default:connection");
     }
 
-    public void updateAppointment( Appointment appointment ) throws SQLException {
-        
+    public void updateAppointment(Appointment appointment, ArrayList<Person> newParticipants, ArrayList<Person> oldParticipants) throws SQLException {
+
         statement = con.prepareStatement("UPDATE appointment SET start=?, end=?, owner=?, description=?, setLocation=?");
         statement.setDate(1, new java.sql.Date(appointment.getStart().getTime()));
         statement.setDate(2, new java.sql.Date(appointment.getEnd().getTime()));
         statement.setString(3, appointment.getOwner().getUsername());
         statement.setString(4, appointment.getDescription());
+
+        con.prepareStatement("INSERT INTO appointment (username, appointmentid) VALUES ( ?, ? )");
+
+        for (Person other : newParticipants) {
+            statement.setString(1, other.getUsername());
+            statement.setString(2, appointment.getId());
+            statement.executeUpdate();
+        }
+        
+        statement = con.prepareStatement("DELETE FROM appointment WHERE username=? AND appointmentid=? )");
+
+        for (Person other : oldParticipants) {
+            statement.setString(1, other.getUsername());
+            statement.setString(2, appointment.getId());
+            statement.executeUpdate();
+
+        }
     }
 }
