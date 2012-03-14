8c0662e6add4e528978f2fee88cd255cfda88d65
lycantrophe <wolfhood@gmail.com>
2012-03-14 09:10:11 +0100

    Moved exception handling to Query class.

--- src/FP/Query.java
+++ src/FP/Query.java
@@ -24,45 +24,77 @@ public class Query {
                 "jdbc:default:connection");
     }
 
-    public void updateAppointment(Appointment appointment, ArrayList<Person> newParticipants, ArrayList<Person> oldParticipants) throws SQLException {
+    public void updateAppointment(Appointment appointment, ArrayList<Person> newParticipants, ArrayList<Person> oldParticipants) {
 
-        statement = con.prepareStatement("UPDATE appointment SET start=?, end=?, owner=?, description=?, setLocation=?");
-        statement.setDate(1, new java.sql.Date(appointment.getStart().getTime()));
-        statement.setDate(2, new java.sql.Date(appointment.getEnd().getTime()));
-        statement.setString(3, appointment.getOwner().getUsername());
-        statement.setString(4, appointment.getDescription());
-        
-        addAppointment( appointment, newParticipants);
+        try {
+            statement = con.prepareStatement("UPDATE appointment SET start=?, end=?, owner=?, description=?, setLocation=?");
+            statement.setDate(1, new java.sql.Date(appointment.getStart().getTime()));
+            statement.setDate(2, new java.sql.Date(appointment.getEnd().getTime()));
+            statement.setString(3, appointment.getOwner().getUsername());
+            statement.setString(4, appointment.getDescription());
+        } catch (SQLException e) {
+            /*
+             * Handle exception
+             */
+        }
 
-        statement = con.prepareStatement("DELETE FROM appointmentRel WHERE username=? AND appointmentid=? )");
+        addAppointment(appointment, newParticipants);
 
-        for (Person other : oldParticipants) {
-            statement.setString(1, other.getUsername());
-            statement.setString(2, appointment.getId());
-            statement.executeUpdate();
+        try {
+            statement = con.prepareStatement("DELETE FROM appointmentRel WHERE username=? AND appointmentid=? )");
+
+            for (Person other : oldParticipants) {
+                statement.setString(1, other.getUsername());
+                statement.setString(2, appointment.getId());
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            /*
+             * Handle exception
+             */
         }
     }
 
-    public void deleteAppointment(Appointment appointment) throws SQLException {
-        statement = con.prepareStatement("DELETE FROM appointmentRel WHERE  appointmentid = ?");
-        statement.setString(1, appointment.getId());
+    public void deleteAppointment(Appointment appointment) {
+        try {
+            statement = con.prepareStatement("DELETE FROM appointmentRel WHERE  appointmentid = ?");
+            statement.setString(1, appointment.getId());
+        } catch (SQLException e) {
+            /*
+             * handle exception
+             */
+        }
     }
 
-    public void updateStatus(String status, Appointment appointment, Person person) throws SQLException {
-        statement = con.prepareStatement("UPDATE appointmentRel SET status = ? WHERE appointmentId = ? AND username = ? ");
-        statement.setString(1, status);
-        statement.setString(2, appointment.getId());
-        statement.setString(3, person.getUsername());
-    }
+    public void updateStatus(String status, Appointment appointment, Person person) {
 
-    public void addAppointment(Appointment appointment, ArrayList<Person> persons) throws SQLException {
-        con.prepareStatement("INSERT INTO appointmentRel (username, appointmentid, status ) VALUES ( ?, ?, ?)");
-
-        for (Person other : persons) {
-            statement.setString(1, other.getUsername());
+        try {
+            statement = con.prepareStatement("UPDATE appointmentRel SET status = ? WHERE appointmentId = ? AND username = ? ");
+            statement.setString(1, status);
             statement.setString(2, appointment.getId());
-            statement.setString(3, "PENDING");
-            statement.executeUpdate();
+            statement.setString(3, person.getUsername());
+        } catch (SQLException e) {
+            /*
+             * Handle exception
+             */
+        }
+    }
+
+    public void addAppointment(Appointment appointment, ArrayList<Person> persons) {
+
+        try {
+            con.prepareStatement("INSERT INTO appointmentRel (username, appointmentid, status ) VALUES ( ?, ?, ?)");
+
+            for (Person other : persons) {
+                statement.setString(1, other.getUsername());
+                statement.setString(2, appointment.getId());
+                statement.setString(3, "PENDING");
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            /*
+             * handle exception
+             */
         }
     }
 
