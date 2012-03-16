970ff32f313a933465c201385ea84063f1067538
lycantrophe <wolfhood@gmail.com>
2012-03-16 19:12:03 +0100

    Sketched flow of initial client data fetch.

--- src/FP/Client.java
+++ src/FP/Client.java
@@ -4,10 +4,54 @@
  */
 package FP;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import no.ntnu.fp.net.co.Connection;
+import no.ntnu.fp.net.co.ConnectionImpl;
+
 /**
  *
  * @author lycantrophe
  */
 public class Client {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Connection connection = new ConnectionImpl(4001);
+        InetAddress addr;
+
+        try {
+            addr = InetAddress.getLocalHost();
+            connection.connect(addr, 5555);
+
+            boolean loginSuccess = false;
+            while (!loginSuccess) {
+                String lg = connection.send(login());
+                if (lg.equals("Login successful")) {
+                    loginSuccess = true;
+                }
+            }
+            connection.receive();
+
+            /*
+             * Build initial connection data
+             */
+
+            /*
+             * get messages
+             */
+
+        } catch (ConnectException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
