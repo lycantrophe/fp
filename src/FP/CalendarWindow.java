715c8dd0662ec06cbcae0b593d3a2c21a49db046
lycantrophe <wolfhood@gmail.com>
2012-03-18 14:05:12 +0100

    Sketched calendarWindow construction flow

---
+++ src/FP/CalendarWindow.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FP;
+
+import java.io.IOException;
+import java.sql.Time;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import no.ntnu.fp.net.co.Connection;
+import java.util.Calendar;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author lycantrophe
+ */
+public class CalendarWindow extends JFrame {
+    
+    private Connection connection;
+    private Person me;
+    private JPanel monday, tuesday, wednesday, thursday, friday, saturday, sunday;
+    private JButton newEventButton;
+    
+    public CalendarWindow(Connection connection) throws IOException, ClassNotFoundException {
+        
+        monday = new JPanel();
+        tuesday = new JPanel();
+        wednesday = new JPanel();
+        thursday = new JPanel();
+        friday = new JPanel();
+        saturday = new JPanel();
+        sunday = new JPanel();
+
+
+        // Determine what week this is
+        //Date date = Calendar.getInstance().getTime();
+
+
+
+
+        /*
+         * Build calendar UI
+         */
+        
+        me = (Person) Server.Deserialize(connection.receive());
+        for (String appId : me.getAppointmentIds()) {
+            /*
+             * Map appointments to the proper days
+             */
+            me.getAppointment(appId);
+        }
+    }
+}
