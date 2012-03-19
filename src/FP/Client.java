118f47d9a28ae24bb3c9347e8be9a68ac060dd53
lycantrophe <lycantrophe@lavabit.com>
2012-03-19 11:53:08 +0100

    Refactored to reflect it's event driven role

--- src/FP/Client.java
+++ src/FP/Client.java
@@ -7,6 +7,7 @@ package FP;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,47 +20,29 @@ import no.ntnu.fp.net.co.ConnectionImpl;
  */
 public class Client {
 
-    private static Person me;
+    private static Connection connection;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
-        Connection connection = new ConnectionImpl(4001);
+        connection = new ConnectionImpl(4001);
         InetAddress addr;
 
         try {
             addr = InetAddress.getLocalHost();
             connection.connect(addr, 5555);
-
-            boolean loginSuccess = false;
-            while (!loginSuccess) {
-                String lg = connection.send(login());
-                if (lg.equals("Login successful")) {
-                    loginSuccess = true;
-                }
-            }
-            try {
-                me = (Person)Server.Deserialize(connection.receive());
-
-                /*
-                 * Build initial connection data
-                 */
-
-                /*
-                 * get messages
-                 */
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
-            }
-            // Create GUI  
-            
-        } catch (ConnectException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
+        Login login = new Login(connection);
+    }
+
+    public void loginSuccessful() throws IOException, ClassNotFoundException {
+        //me = (Person) Server.Deserialize(connection.receive());
+        CalendarWindow calwin = new CalendarWindow(connection);
     }
 }
