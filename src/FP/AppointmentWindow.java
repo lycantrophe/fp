68fd8b485729ceb10b60b50f4324607a30a500ab
adamczyk <kamiladamczyk@hotmail.com>
2012-03-20 18:28:03 +0100

    Appointment Window GUI, no action

--- src/FP/AppointmentWindow.java
+++ src/FP/AppointmentWindow.java
@@ -4,6 +4,8 @@
  */
 package FP;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -14,34 +16,48 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
+import javax.swing.JSpinner.DateEditor;
+import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
+
 import no.ntnu.fp.net.co.Connection;
 
 /**
- *
  * @author lycantrophe
  */
+
 public class AppointmentWindow extends JFrame {
 
     protected Connection connection;
-    protected JTextField textDescription;
+    protected JTextField txtDescription, txtLocation;
     protected JSpinner spinnerStartDate, spinnerEndDate, spinnerStartTime, spinnerEndTime;
-    protected JButton buttonInvite, buttonAddParticipant, buttonNewLocation, buttonSave, buttonCancel;
-    protected JComboBox comboLocations;
+    protected JButton buttonInvite, buttonAddParticipant, btnReserveRoom, buttonSave, buttonCancel;
     protected ArrayList<Person> invited;
     protected ArrayList<String> participants;
     protected ArrayList<Location> locations;
     protected Person me;
     protected Location location;
     protected appWinListener al;
+    
 
-    public AppointmentWindow(Connection connection, Person me) {
-
+    public AppointmentWindow(Connection connection, Person me)
+    {
         this.connection = connection;
-        Date date = new Date();
-        al = new appWinListener();
         this.me = me;
-        participants = new ArrayList<String>();
+        
+        this.al = new appWinListener();
+        this.participants = new ArrayList<String>();
+        this.invited = new ArrayList<Person>();
+
         try {
             connection.send("getLocations");
             locations = (ArrayList<Location>) Server.Deserialize(connection.receive());
@@ -51,44 +67,98 @@ public class AppointmentWindow extends JFrame {
             Logger.getLogger(AppointmentWindow.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(AppointmentWindow.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NullPointerException ex) {
+        	locations = new ArrayList<Location>();
         }
-
-        this.textDescription = new JTextField();
-
-        // Date pickers
-        this.spinnerStartDate = new JSpinner(new SpinnerDateModel());
-        this.spinnerEndDate = new JSpinner(new SpinnerDateModel());
-        this.spinnerStartTime = new JSpinner(new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY));
-        this.spinnerEndTime = new JSpinner(new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY));
-
-
-        this.buttonInvite = new JButton();
-        this.buttonAddParticipant = new JButton();
-        this.buttonNewLocation = new JButton();
-        this.buttonSave = new JButton();
-        this.buttonCancel = new JButton();
-
-        invited = new ArrayList<Person>();
-        this.comboLocations = new JComboBox((Location[]) locations.toArray());
-
-        textDescription.setToolTipText("Event description");
-
-        // Create time spinners and editors
-        JSpinner.DateEditor timeSpinnerStart = new JSpinner.DateEditor(spinnerStartTime, "hh:mm");
-        JSpinner.DateEditor timeSpinnerEnd = new JSpinner.DateEditor(spinnerStartTime, "hh:mm");
-
-        spinnerStartTime.setEditor(timeSpinnerStart);
-        spinnerEndTime.setEditor(timeSpinnerEnd);
-
-        buttonInvite.setText("Invite people");
+        
+        InitializeGUI();
+    }
+    
+    public void InitializeGUI()
+    {
+    	Date date = new Date();
+    	
+    	txtDescription = new JTextField(16);
+    	txtDescription.setToolTipText("Event description");
+    	spinnerStartDate = new JSpinner(new SpinnerDateModel());
+    	spinnerStartDate.setEditor(new DateEditor(spinnerStartDate, "dd.mm.yy"));
+    	spinnerStartTime = new JSpinner(new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY));
+    	spinnerStartTime.setEditor(new DateEditor(spinnerStartTime, "kk:mm"));
+        spinnerEndDate = new JSpinner(new SpinnerDateModel());
+        spinnerEndDate.setEditor(new DateEditor(spinnerEndDate, "dd.mm.yy"));
+        spinnerEndTime = new JSpinner(new SpinnerDateModel(date, null, null, Calendar.HOUR_OF_DAY));
+        spinnerEndTime.setEditor(new DateEditor(spinnerEndTime, "kk:mm"));
+    	txtLocation = new JTextField(16);
+        btnReserveRoom = new JButton("Reserver rom");
+        btnReserveRoom.addActionListener(al);
+        buttonInvite = new JButton("Invite people");
         buttonInvite.addActionListener(al);
-
-        buttonNewLocation.setText("Add location");
-        buttonNewLocation.addActionListener(al);
-        comboLocations.addActionListener(al);
-
-        buttonAddParticipant.setText("Add external participants");
+        buttonAddParticipant = new JButton("Add external participants");
         buttonAddParticipant.addActionListener(al);
+        buttonSave = new JButton("Save");
+        buttonCancel = new JButton("Cancel");
+        
+        JPanel des = new JPanel();
+        des.add(txtDescription);
+        
+        JPanel start = new JPanel();
+        start.add(spinnerStartDate);
+        start.add(spinnerStartTime);
+        
+        JPanel end = new JPanel();
+        end.add(spinnerEndDate);
+        end.add(spinnerEndTime);
+        
+        JPanel loc = new JPanel();
+        loc.add(txtLocation);
+        loc.add(btnReserveRoom);
+        
+        JPanel par = new JPanel();
+        par.add(buttonInvite);
+        par.add(buttonAddParticipant);
+        
+        JPanel big = new JPanel();
+        big.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 0;
+        c.gridy = 0;
+        big.add(new JLabel("Beskrivelse:"), c);
+        c.gridy = 1;
+        big.add(new JLabel("Fra:"), c);
+        c.gridy = 2;
+        big.add(new JLabel("Til:"), c);
+        c.gridy = 3;
+        big.add(new JLabel("Sted:"), c);
+        c.gridy = 4;
+        big.add(new JLabel("Deltakere:"), c);
+        c.gridx = 1;
+        c.gridy = 0;
+        big.add(des, c);
+        c.gridy = 1;
+        big.add(start, c);
+        c.gridy = 2;
+        big.add(end, c);
+        c.gridy = 3;
+        big.add(loc, c);
+        c.gridy = 4;
+        big.add(par, c);
+        
+        JPanel buttons = new JPanel();
+        buttons.add(buttonSave);
+        buttons.add(buttonCancel);
+        
+        JPanel container = new JPanel();
+        container.setLayout(new GridBagLayout());
+        c.anchor = GridBagConstraints.FIRST_LINE_START;
+        c.gridx = 0;
+        c.gridy = 0;
+        container.add(big, c);
+        c.anchor = GridBagConstraints.LAST_LINE_END;
+        c.gridy = 1;
+        container.add(buttons, c);
+        
+        this.add(container);
     }
 
     public void sendEditAppointment() {
@@ -97,7 +167,7 @@ public class AppointmentWindow extends JFrame {
         Date endDate = (Date) spinnerEndDate.getValue();
         // TODO: Get values from fields
 
-        String description = textDescription.getText();
+        String description = txtDescription.getText();
         
         ArrayList<Attending> attending = new ArrayList<Attending>();
         for( Person other : invited ){
@@ -117,19 +187,17 @@ public class AppointmentWindow extends JFrame {
         // Add appointment to calendar
     }
 
-    protected class appWinListener implements ActionListener {
-
+    protected class appWinListener implements ActionListener
+    {
         public void actionPerformed(ActionEvent ae) {
             if (ae.getSource() == buttonInvite) {
                 InvitePeople inv = new InvitePeople();
                 // TODO: Make window visible (and later modal if possible)
-            } else if (ae.getSource() == buttonNewLocation) {
+            } else if (ae.getSource() == btnReserveRoom) {
                 // TODO: Implement Location creation and selection
             } else if (ae.getSource() == buttonAddParticipant) {
                 // TODO: Implement participant creation
-            } else if (ae.getSource() == comboLocations) {
-                location = (Location) comboLocations.getSelectedItem();
-            } else if (ae.getSource() == buttonSave) {
+            }else if (ae.getSource() == buttonSave) {
                 sendEditAppointment();
                 dispose();
             } else if (ae.getSource() == buttonCancel) {
